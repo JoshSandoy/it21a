13ab8c52c4a4448151c2613462f232f0d2264a03
JoshSandoy <leeken851@gmail.com>
2025-09-15 08:36:49 +0800

    added mage

--- hero.js
+++ hero.js
@@ -18,5 +18,18 @@ class Warriror extends Hero{
     }
 }
 
+class Mage extends Hero{
+constructor(name,health,attack,mana){
+    super(name,health,attack);
+    this.mana = mana;
+}
+useAbility(){
+    console.log('$(this.name) casts Fireball!');
+}
+}
+
 const warrior = new Warriror("Thorin", 100,10);
 console.log(warrior.useAbility());
+
+const mage = new Mage("Gandalf", 80,8,50);
+mage.useAbility();
